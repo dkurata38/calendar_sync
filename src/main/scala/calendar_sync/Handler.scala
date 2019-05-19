fcde097d8357b583ca79f53eeea119c4efc09761
DaisukeKURATA <d.kruata.38@gmail.com>
2019-05-20 00:34:48 +0900

    不要なクラスを削除. batchパッケージの実装内容をHandlerに実装

--- src/main/scala/calendar_sync/Handler.scala
+++ src/main/scala/calendar_sync/Handler.scala
@@ -1,13 +1,22 @@
 package calendar_sync
 
-import calendar_sync.batch.CalendarSyncExecutor
+import calendar_sync.application.coordinator.EventsSyncCoordinator
+import calendar_sync.domain.Date
 import com.amazonaws.services.lambda.runtime.Context
 import com.google.inject.Guice
+import com.typesafe.config.ConfigFactory
+
+import scala.collection.JavaConverters.collectionAsScalaIterableConverter
 
 class Handler {
   def handleRequest(input: String, context: Context): Unit = {
+    val config = ConfigFactory.load()
+    val sourceCalendarIds = config.getStringList("app.calendar.source").asScala.toSeq
+    val targetCalendarId = config.getString("app.calendar.target")
+    val dummyText = config.getString("app.dummyText")
     val injector = Guice.createInjector(new CalendarSyncModule)
-    val executor = injector.getInstance(classOf[CalendarSyncExecutor])
-    executor.execute
+
+    val coordinator = injector.getInstance(classOf[EventsSyncCoordinator])
+    coordinator.sync(sourceCalendarIds, targetCalendarId, Date.today + 14, dummyText)
   }
 }
