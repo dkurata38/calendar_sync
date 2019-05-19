a9da8398b420c864486b04d09b36e5585324cabc
DaisukeKURATA <d.kruata.38@gmail.com>
2019-05-19 19:00:43 +0900

    カレンダーのタイトルはStatusじゃなくてSummaryだったことが判明. 直しました.

--- src/main/scala/calendar_sync/domain/Event.scala
+++ src/main/scala/calendar_sync/domain/Event.scala
@@ -5,10 +5,10 @@ case class Event(value: com.google.api.services.calendar.model.Event) {
   def id = value.getId
   def mask(dummyText: String) = Event(
     new com.google.api.services.calendar.model.Event()
-      .setStatus(dummyText)
+      .setSummary(dummyText)
       .setStart(value.getStart)
       .setEnd(value.getEnd)
   )
 
-  def masked(dummyText: String) = value.getStatus == dummyText
+  def masked(dummyText: String) = value.getSummary == dummyText
 }
