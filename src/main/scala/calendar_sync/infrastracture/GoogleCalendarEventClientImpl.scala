7f2ffb3fba2f9230704c691b6be1d37fa7ae244e
DaisukeKURATA <d.kruata.38@gmail.com>
2019-05-19 11:34:37 +0900

    不要なインポートを削除

--- src/main/scala/calendar_sync/infrastracture/GoogleCalendarEventClientImpl.scala
+++ src/main/scala/calendar_sync/infrastracture/GoogleCalendarEventClientImpl.scala
@@ -8,7 +8,7 @@ import calendar_sync.domain.{Date, Duration, Event, GoogleCalendarEventClient}
 import com.google.api.client.auth.oauth2.Credential
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver
-import com.google.api.client.googleapis.auth.oauth2.{GoogleAuthorizationCodeFlow, GoogleClientSecrets, GoogleCredential}
+import com.google.api.client.googleapis.auth.oauth2.{GoogleAuthorizationCodeFlow, GoogleClientSecrets}
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
 import com.google.api.client.json.jackson2.JacksonFactory
 import com.google.api.client.util.DateTime
