f4bc5104bfc623d49d8694bd3873a808079c2527
DaisukeKURATA <d.kruata.38@gmail.com>
2019-05-26 20:24:14 +0900

    リファクタリング

---
+++ src/main/scala/calendar_sync/infrastracture/google/GoogleApiClient.scala
@@ -0,0 +1,21 @@
+package calendar_sync.infrastracture.google
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.model.{HttpResponse, StatusCode}
+import akka.stream.ActorMaterializer
+import akka.util.ByteString
+import calendar_sync.infrastracture.google.client_secrets.ClientSecrets
+
+import scala.concurrent.{ExecutionContext, Future}
+
+trait GoogleApiClient {
+  implicit val actorSystem: ActorSystem = ActorSystem()
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
+  implicit val executionContext: ExecutionContext = scala.concurrent.ExecutionContext.Implicits.global
+  val clientSecrets: ClientSecrets = ClientSecretsFile.load
+
+  object Response {
+    def unapply(httpResponse: HttpResponse): Option[(StatusCode, Future[String])] =
+      Some(httpResponse.status, httpResponse.entity.dataBytes.runFold(ByteString.empty)(_ ++ _).map(bs => bs.utf8String))
+  }
+}
--- src/main/scala/calendar_sync/infrastracture/google/oauth/GoogleOauthClient.scala
+++ src/main/scala/calendar_sync/infrastracture/google/oauth/GoogleOauthClient.scala
@@ -1,26 +1,19 @@
 package calendar_sync.infrastracture.google.oauth
 
-1import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives
 import akka.http.scaladsl.unmarshalling.Unmarshal
-import akka.stream.ActorMaterializer
-import akka.util.ByteString
 import calendar_sync.domain.credential.{Credential, OAuthClient, RefreshedAccessToken}
-import calendar_sync.infrastracture.google.ClientSecretsFile
+import calendar_sync.infrastracture.google.GoogleApiClient
 import javax.inject.Singleton
 
 import scala.concurrent.duration.Duration
-import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.concurrent.{Await, Future}
 import scala.util.{Failure, Success, Try}
 
 @Singleton
-class GoogleOauthClient extends Directives with OAuthResponseJsonSupport with OAuthClient{
-  private implicit val actorSystem: ActorSystem = ActorSystem()
-  private implicit val materializer: ActorMaterializer = ActorMaterializer()
-  private implicit val executionContext: ExecutionContext = scala.concurrent.ExecutionContext.Implicits.global
-  private val clientSecrets = ClientSecretsFile.load
+class GoogleOauthClient extends Directives with OAuthResponseJsonSupport with OAuthClient with GoogleApiClient {
   def refreshAccessToken(credential: Credential) = {
     val formData = FormData(
       "client_id" -> clientSecrets.clientId,
@@ -40,10 +33,5 @@ class GoogleOauthClient extends Directives with OAuthResponseJsonSupport with OA
     }
 
     Try(Await.result(maybeRefreshedToken, Duration.Inf))
-  }
-
-  object Response {
-    def unapply(httpResponse: HttpResponse): Option[(StatusCode, Future[String])] =
-      Some(httpResponse.status, httpResponse.entity.dataBytes.runFold(ByteString.empty)(_ ++ _).map(bs => bs.utf8String))
   }
 }
