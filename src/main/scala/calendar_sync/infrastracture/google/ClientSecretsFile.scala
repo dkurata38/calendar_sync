c20b2c7d33c6f73badc9a15a98b8d206a71e5e31
DaisukeKURATA <d.kruata.38@gmail.com>
2019-05-26 18:31:33 +0900

    client_secrets.jsonを読み込むクラスを作成

---
+++ src/main/scala/calendar_sync/infrastracture/google/ClientSecretsFile.scala
@@ -0,0 +1,19 @@
+package calendar_sync.infrastracture.google
+
+import calendar_sync.infrastracture.google.client_secrets.{ClientSecrets, ClientSecretsProtocol}
+import spray.json.enrichString
+
+import scala.io.{Codec, Source}
+
+case class ClientSecretsFile(clientSecrets: ClientSecrets)
+
+case object ClientSecretsFile extends ClientSecretsProtocol {
+  private val clientSecretsJson = Source
+    .fromInputStream(getClass.getResourceAsStream("/token/client_secrets.json"), Codec.UTF8.name)
+    .getLines()
+    .mkString(" ")
+    .parseJson.convertTo[ClientSecretsFile]
+    .clientSecrets
+
+  def load: ClientSecrets = clientSecretsJson
+}
---
+++ src/main/scala/calendar_sync/infrastracture/google/client_secrets/ClientSecrets.scala
@@ -0,0 +1,3 @@
+package calendar_sync.infrastracture.google.client_secrets
+
+case class ClientSecrets(clientId: String, clientSecret: String)
