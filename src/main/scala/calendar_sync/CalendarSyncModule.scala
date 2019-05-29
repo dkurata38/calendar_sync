2e64cb25deb89c900c86e4aa13cdcf0d95c28aa7
DaisukeKURATA <d.kruata.38@gmail.com>
2019-05-29 22:03:09 +0900

    Injection時のエラーを修正

--- src/main/scala/calendar_sync/CalendarSyncModule.scala
+++ src/main/scala/calendar_sync/CalendarSyncModule.scala
@@ -1,13 +1,15 @@
 package calendar_sync
 
 import calendar_sync.domain.CalendarEventClient
-import calendar_sync.domain.credential.CredentialRepository
+import calendar_sync.domain.credential.{CredentialRepository, OAuthClient}
 import calendar_sync.infrastracture.GoogleCalendarEventClientImpl
 import calendar_sync.infrastracture.dynamo_db.CredentialDynamoDBRepository
+import calendar_sync.infrastracture.google.oauth.GoogleOauthClient
 import com.google.inject.AbstractModule
 
 class CalendarSyncModule extends AbstractModule {
   override def configure(): Unit = {
+    bind(classOf[OAuthClient]).to(classOf[GoogleOauthClient])
     bind(classOf[CalendarEventClient]).to(classOf[GoogleCalendarEventClientImpl])
     bind(classOf[CredentialRepository]).to(classOf[CredentialDynamoDBRepository])
   }
